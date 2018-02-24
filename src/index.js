08af42a43474b91aa82e6f1fe9f0023db1bcbe8f
IgorDive <ihar.kazlovich@gmail.com>
2018-02-24 11:31:58 +0300

    first implementation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
+ * @param prf - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+module.exports = function getLoveTrianglesCount(prf = []) {
+  let count = 0;
+
+  for (i = 0; i < prf.length; i++) {
+    if ((prf[prf[prf[i] - 1] -1] - 1 === i) && (prf[i] - 1 > i) && (prf[prf[i] - 1] - 1 > i)) count++;
+  };
+  return count;
 };
